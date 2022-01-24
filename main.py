c8434c50af7de3656c7de2949bdd2f787c52264c
Jigyasu <jigyasunigam19@gmail.com>
2022-01-24 15:01:47 +0530

    5 lives added

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import random
 
 def play():
-    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
-    computer = random.choice(['r', 'p', 's'])
-
-    if user == computer:
-        return 'It\'s a tie'
-
-    # r > s, s > p, p > r
-    if is_win(user, computer):
-        return 'You won!'
-
-    return 'You lost!'
+    l=5
+    w=5
+    while(l>0 and w>0):
+        user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+        computer = random.choice(['r', 'p', 's'])
+        print(computer)
+        if user == computer:
+           print('It\'s a tie you have',w,"lives left")
+           # r > s, s > p, p > r
+        if is_win(user, computer):
+           l-=1
+           print('You win and you have',w,'lives left. And the computer have',l,'lives left')
+        if(is_win(user, computer)==False):
+           w-=1
+           print('Computer wins and you have',w,'lives left.And the computer have',l,'lives left')
+        if(w==0):
+            return ' GAME OVER YOU LOST!'
+        if(l==0):
+            return 'GAME OVER YOU WIN!'
 
 def is_win(player, opponent):
     # return true if player wins
@@ -19,5 +27,7 @@ def is_win(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
         or (player == 'p' and opponent == 'r'):
         return True
+    elif(player!=opponent):
+        return False
 
 print(play())
